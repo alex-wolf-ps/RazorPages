e9243266e50272fb687a8b0fb7b0eee79b028463
Alex Wolf <alexrudiwolf@gmail.com>
2018-08-29 22:11:52 -0400

    Initial commit

---
+++ WiredBrainCoffee/Pages/Menu.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
